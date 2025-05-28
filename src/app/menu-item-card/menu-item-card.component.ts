aaafb1cbcdb7f73bb5c10af37f19b8898dd644fe
Adam <adam.fafera@edu.uekat.pl>
2025-05-29 00:04:59 +0200

    Add reactive dialog boxes to menu items (WIP)

--- src/app/menu-item-card/menu-item-card.component.ts
+++ src/app/menu-item-card/menu-item-card.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { TabsModule } from 'primeng/tabs';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-menu-item-card',
-  imports: [CardModule, ButtonModule ],
+  imports: [CardModule, ButtonModule, DialogModule, TabsModule, TableModule],
   templateUrl: './menu-item-card.component.html',
   styleUrl: './menu-item-card.component.css'
 })
 export class MenuItemCardComponent {
+  visible : boolean = false;
 
+  showDialog() {
+    this.visible = true;
+    console.log('Clicked on the card');
+  }
+
+  products = [
+    {
+      name: 'Calories (kcal)',
+      category: 280,
+    },
+    {
+      name: 'Fats (g)',
+      category: 9,
+    },
+    {
+      name: 'Carbohydrates (g)',
+      category: 34,
+    },
+    {
+      name: 'Protein (g)',
+      category: 15,
+    },
+
+  ];
 }
