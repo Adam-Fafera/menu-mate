2df57d3b6f336c40c072cbd1aaa05d1f652cfad0
Adam <adam.fafera@edu.uekat.pl>
2025-05-29 22:39:14 +0200

    Add kinda functional post review button with toast and review tab to menu items

--- src/app/menu-item-card/menu-item-card.component.ts
+++ src/app/menu-item-card/menu-item-card.component.ts
@@ -4,15 +4,18 @@ import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { TabsModule } from 'primeng/tabs';
 import { TableModule } from 'primeng/table';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-menu-item-card',
-  imports: [CardModule, ButtonModule, DialogModule, TabsModule, TableModule],
+  imports: [CardModule, ButtonModule, DialogModule, TabsModule, TableModule, RatingModule, FormsModule],
   templateUrl: './menu-item-card.component.html',
   styleUrl: './menu-item-card.component.css'
 })
 export class MenuItemCardComponent {
   visible : boolean = false;
+  value : number = 4;
 
   showDialog() {
     this.visible = true;
