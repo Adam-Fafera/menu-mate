4faf68cfa22cb29b65c803d2d0c52ac3e373be58
Adam <adam.fafera@edu.uekat.pl>
2025-06-01 01:50:10 +0200

    Clean up restaurant page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'white-sm': '0 1px 2px 0 rgba(255, 255, 255, 0.05)',
+        'white-md': '0 4px 6px -1px rgba(255, 255, 255, 0.1)',
+        'white-lg': '0 10px 15px -3px rgba(255, 255, 255, 0.1)',
+        'white-xl': '0 20px 25px -5px rgba(255, 255, 255, 0.1)',
+        'white-2xl': '0 25px 50px -12px rgba(255, 255, 255, 0.25)',
+        'white-inner': 'inset 0 2px 4px 0 rgba(255, 255, 255, 0.05)',
+      }
+    },
   },
   plugins: [],
 }
