8b55c8c01fc9a89462bb77c68f920d2b0924beff
Adam <adam.fafera@edu.uekat.pl>
2025-06-11 14:59:58 +0200

    Modify cuisine change in restaurant dashboard

--- frontend/src/app/restaurant-dashboard-restaurant-self-manage/restaurant-dashboard-restaurant-self-manage.component.ts
+++ frontend/src/app/restaurant-dashboard-restaurant-self-manage/restaurant-dashboard-restaurant-self-manage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { FileUploadModule } from 'primeng/fileupload';
@@ -10,28 +10,47 @@ import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { InputMaskModule } from 'primeng/inputmask';
 import { InputTextModule } from 'primeng/inputtext';
 import { TextareaModule } from 'primeng/textarea';
+import { MultiSelectModule } from 'primeng/multiselect';
 
 @Component({
   selector: 'app-restaurant-dashboard-restaurant-self-manage',
   imports: [IftaLabel, FormsModule, FileUploadModule, SideMenuComponent, ButtonModule, ToastModule, ConfirmDialogModule, InputMaskModule,
-            InputTextModule, TextareaModule],
+            InputTextModule, TextareaModule, MultiSelectModule],
   standalone:true,
   templateUrl: './restaurant-dashboard-restaurant-self-manage.component.html',
   styleUrl: './restaurant-dashboard-restaurant-self-manage.component.css',
   providers: [ConfirmationService, MessageService]
 })
 
-export class RestaurantDashboardRestaurantSelfManageComponent {
+export class RestaurantDashboardRestaurantSelfManageComponent implements OnInit {
 
     name: string | undefined;
     description: string | undefined;
     phone: string | undefined;
     location: string | undefined;
-    cuisine: string | undefined;
+    cuisines: any[] | undefined;
     uploadedFiles: any[] = [];
+    selectedCuisine: string | undefined;
   
     constructor(private confirmationService: ConfirmationService, private messageService: MessageService) {}
   
+    ngOnInit(): void {
+        this.cuisines = [
+        'Fast Food',
+        'Pizza',
+        'Burgers',
+        'Sushi',
+        'Italian',
+        'Asian',
+        'American',
+        'Soup',
+        'Indian',
+        'Desserts',
+        'Japanese',
+        'Comfort Food'
+        ];
+    }
+
     confirm(event: Event) {
         this.confirmationService.confirm({
             target: event.target as EventTarget,
