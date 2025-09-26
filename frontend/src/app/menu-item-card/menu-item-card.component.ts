871551f0e9df75d415327d2ca70091209295476b
Adam <adam.fafera@edu.uekat.pl>
2025-09-26 23:11:43 +0200

    Add item reviews

--- frontend/src/app/menu-item-card/menu-item-card.component.ts
+++ frontend/src/app/menu-item-card/menu-item-card.component.ts
@@ -9,6 +9,7 @@ import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ReviewItemComponent } from "../review-item/review-item.component";
 import { ProposeItemEditComponent} from '../propose-item-edit/propose-item-edit.component';
+import { ReviewItemMakeComponent } from "../review-item-make/review-item-make.component";
 
 
 enum Currency {
@@ -19,13 +20,14 @@ enum Currency {
 
 @Component({
   selector: 'app-menu-item-card',
-  imports: [CardModule, ButtonModule, DialogModule, TabsModule, TableModule, RatingModule, FormsModule, ReviewItemComponent, ProposeItemEditComponent],
+  imports: [CardModule, ButtonModule, DialogModule, TabsModule, TableModule, RatingModule, FormsModule, ReviewItemComponent, ProposeItemEditComponent, ReviewItemMakeComponent],
   templateUrl: './menu-item-card.component.html',
   styleUrl: './menu-item-card.component.css'
 })
 export class MenuItemCardComponent {
   visible : boolean = false;
   visibleEdit : boolean = false;
+  visibleReview : boolean = false;
   items : any[] = [];
   value : number = 4;
   selectedItem = signal<any | null>(null);
@@ -44,6 +46,14 @@ export class MenuItemCardComponent {
 
   hideEdit(){
     this.visibleEdit = false;
+  }
+
+  showReview(){
+    this.visibleReview = true;
+  }
+  
+  hideReview(){
+    this.visibleReview = false;
   }
 
   getCurrencySymbol(currency: Currency): string {
--- frontend/src/app/review-item-make/review-item-make.component.ts
+++ frontend/src/app/review-item-make/review-item-make.component.ts
@@ -1,11 +1,72 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { RatingModule } from 'primeng/rating';
+import { MessageService } from 'primeng/api';
+import { HttpClient } from '@angular/common/http';
+import { Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TextareaModule } from 'primeng/textarea';
+
 
 @Component({
   selector: 'app-review-item-make',
-  imports: [],
+  imports: [DialogModule,RatingModule, FloatLabelModule, ButtonModule, FormsModule, TextareaModule],
   templateUrl: './review-item-make.component.html',
   styleUrl: './review-item-make.component.css'
 })
 export class ReviewItemMakeComponent {
+
+  @Input() visible: boolean = false;
+  @Input() itemId: string | null = null;
+  @Output() visibleChange = new EventEmitter<boolean>();
+  
+  title: string ='';
+  description: string = '';
+  rating: number = 0;
+
+  constructor(public messageService: MessageService, public http: HttpClient, private route: ActivatedRoute) {}
+
+  
+  postReview() {
+
+    const review = {
+      itemId: this.itemId,
+      userId: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      userName: "Kiciula27",
+      userImagePath: "https://innostudio.de/fileuploader/images/default-avatar.png",
+      title: this.title,
+      description: this.description,
+      rating: this.rating 
+    }
+
+    this.http.post(`https://localhost:7084/api/ReviewItems`, review)
+      .subscribe({
+        next: (response) => {
+          console.log('Review posted successfully ', response);
+          this.messageService.add({severity:'success', summary: "Success", detail:"Review posted"});
+          this.closeDialog();
+          this.resetForm();
+        },
+        error: (error) =>{
+          console.error('Error posting review', error);
+          this.messageService.add({severity:'error', summary: "Error", detail:"Failed to post review"});
+        }
+      })
+
+  }
+
+  closeDialog() {
+    this.visible = false;
+    this.visibleChange.emit(this.visible);
+  }
+
+  resetForm() {
+    this.title = '';
+    this.description = '';
+    this.rating = 0;
+  }
 
 }
